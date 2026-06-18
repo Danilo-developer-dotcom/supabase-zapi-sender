dd11c11ee8b4fb4faf198a8c32492cb949ca04fe
DaniloDev <daniloemanuel684@gmail.com>
2026-06-18 15:57:46 -0300

    Finalização do Código

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 from src.db import Supabase
 from src.zapi import Zapi
+from dotenv import load_dotenv
 
+load_dotenv()
 supabase = Supabase()
 zapi = Zapi()
 
+# lista de contatos em formato de dicionário
 contacts_list = supabase.read()
 
 for contact in contacts_list:
     message = f"Olá, {contact["name"]} tudo bem com você?"
     zapi.send(message, contact["phone"])
-
