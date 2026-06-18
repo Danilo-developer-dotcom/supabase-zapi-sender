e7876eed741dbc14e32d8a15dd8e367a92b38a6d
DaniloDev <daniloemanuel684@gmail.com>
2026-06-18 15:16:18 -0300

    unifica recursos e automatiza disparo de mensagens para os contatos do Banco de Dados

--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 from src.db import Supabase
+from src.zapi import Zapi
 
 supabase = Supabase()
+zapi = Zapi()
 
-lista_contatos = supabase.read()
+contacts_list = supabase.read()
+
+for i in range(len(contacts_list)):
+    name = contacts_list[i]["name"]
+    phone = contacts_list[i]["phone"]
+    message = f"Olá, {name} tudo bem com você?"
+    zapi.send(message, phone)
+
