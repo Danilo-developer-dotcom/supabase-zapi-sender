a46514f03d642bcfcc8e0a946e472c4c2ad03d9c
DaniloDev <daniloemanuel684@gmail.com>
2026-06-18 15:55:55 -0300

    validação de variáveis de ambiente Supabase

--- src/db.py
+++ src/db.py
@@ -1,11 +1,16 @@
 from supabase import create_client, Client
 import os
 
+
 class Supabase:
     def __init__(self):
         # Carrega as variáveis de ambiente e conecta ao Banco de Dados
         self.SUPABASE_URL: str = os.getenv("SUPABASE_URL")
         self.SUPABASE_KEY: str = os.getenv("SUPABASE_KEY")
+
+        # Validação de variáveis de ambiente
+        if not self.SUPABASE_KEY or not self.SUPABASE_URL:
+            raise ValueError("Variáveis SUPABASE_URL e SUPABASE_KEY são obrigatórias.")
         self.supabase: Client = create_client(self.SUPABASE_URL, self.SUPABASE_KEY)
 
     def read(self) -> list:
